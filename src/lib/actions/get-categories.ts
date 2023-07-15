33706c726f994e62b3d810ad5bfd08ca203dbde5
salmansheri <sheriffsalman00@gmail.com>
2023-07-15 17:36:01 +0530

    fix: fixed the url

--- src/lib/actions/get-categories.ts
+++ src/lib/actions/get-categories.ts
@@ -1,7 +1,7 @@
 import { CategoryInterface } from "@/types";
 import axios from "axios";
 
-const URL = `http://localhost:3001/api/64a8637f2587d6674e757ba2/categories`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 export default async function getCategories(): Promise<CategoryInterface[]> {
   const response = await axios(URL);
